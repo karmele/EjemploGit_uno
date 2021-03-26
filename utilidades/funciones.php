622e97d02d406ff9d811d06d1ad7b580472d9034
Karmele Arocena <dweskarmele2@gmail.com>
2021-03-26 09:42:49 +0100

    primera función

---
+++ utilidades/funciones.php
@@ -0,0 +1,10 @@
+<?php 
+
+      /*
+       * función que calcula el área de un cuadrado
+       */
+      function calcularArea($lado)
+      {
+          $area = $lado * $lado;
+          return $area;
+      }
